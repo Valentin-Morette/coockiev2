886c3cfe65e0d0bd6c49cb677802005370183e36
MORETTE Valentin <morette.valentin@gmail.com>
2022-06-10 17:37:24 +0200

    add cps and cpp

--- src/App.js
+++ src/App.js
@@ -1,62 +1,95 @@
-import './App.css';
+import "./App.css";
 import { useState, useEffect } from "react";
 import coockie from "./assets/img/coockie.png";
-import MiniCookie from './components/MiniCookie';
-import { motion } from "framer-motion"
+import MiniCookie from "./components/MiniCookie";
+import { motion } from "framer-motion";
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(1000);
+  const [cpp, setCpp] = useState(1);
+  const [cps, setCps] = useState(1);
 
-  useEffect( () => {
-
-    setInterval( () => {
-        setCounter(count => count + 1);
-    }, 1000)
-
-  },[])
+  useEffect(() => {
+    const myTimer = setInterval(() => {
+      setCounter((counter) => counter + cps);
+      console.log("Interval Launch");
+    }, 1000);
+    return () => clearInterval(myTimer);
+  }, [cps]);
 
   const modifier = (color) => {
     if (counter >= 10 && color === "white") {
       setCounter(counter - 10);
+      setCpp(cpp + 1);
     }
     if (counter >= 100 && color === "black") {
-        setCounter(counter - 100);
+      setCounter(counter - 100);
+      setCpp(cpp + 10);
     }
     if (counter >= 1000 && color === "cyan") {
-        setCounter(counter - 1000);
+      setCounter(counter - 1000);
+      setCpp(cpp + 100);
     }
     if (counter >= 100 && color === "green") {
-        setCounter(counter - 100);
+      setCounter(counter - 100);
+      setCps(cps + 1);
     }
     if (counter >= 1000 && color === "yellow") {
-        setCounter(counter - 1000);
+      setCounter(counter - 1000);
+      setCps((prev) => prev + 10);
     }
     if (counter >= 10000 && color === "red") {
-        setCounter(counter - 10000);
+      setCounter(counter - 10000);
+      setCps(cps + 100);
     }
-  }
+  };
 
   return (
-    <div className='all'>
+    <div className="all">
       <h1>Cookie-Clicker</h1>
-      <ul className='stats'>
+      <ul className="stats">
         <li>{counter}</li>
-        <li>Cookie per sec</li>
-        <li>Cookie per click</li>
+        <li>{cpp} 🍪🖱️</li>
+        <li>{cps} 🍪⏲️</li>
       </ul>
-      <button className='mainButton' onClick={() => setCounter(count => count + 1)}>
-        <motion.img className='mainCookie' draggable="false" src= {coockie} alt="main coockie"
-          whileHover={{ scale: 1.1 }}
+      <button
+        className="mainButton"
+        onClick={() => setCounter((count) => count + cpp)}
+      >
+        <motion.img
+          className="mainCookie"
+          draggable="false"
+          src={coockie}
+          alt="main coockie"
           whileTap={{ scale: 0.9 }}
         />
       </button>
-      <div className='allSecondButton'>
-        <MiniCookie booster={() => modifier("white")} color = "white" valor = {1}/>
-        <MiniCookie booster={() => modifier("black")} color = "black" valor = {10}/>
-        <MiniCookie booster={() => modifier("cyan")} color = "cyan" valor = {100}/>
-        <MiniCookie booster={() => modifier("green")} color = "green" valor = {1} multiple={true}/>
-        <MiniCookie booster={() => modifier("yellow")} color = "yellow" valor = {10} multiple={true}/>
-        <MiniCookie booster={() => modifier("red")} color = "red" valor = {100} multiple={true}/>
+      <div className="allSecondButton">
+        <MiniCookie booster={() => modifier("white")} color="white" valor={1} />
+        <MiniCookie
+          booster={() => modifier("black")}
+          color="black"
+          valor={10}
+        />
+        <MiniCookie booster={() => modifier("cyan")} color="cyan" valor={100} />
+        <MiniCookie
+          booster={() => modifier("green")}
+          color="green"
+          valor={1}
+          multiple={true}
+        />
+        <MiniCookie
+          booster={() => modifier("yellow")}
+          color="yellow"
+          valor={10}
+          multiple={true}
+        />
+        <MiniCookie
+          booster={() => modifier("red")}
+          color="red"
+          valor={100}
+          multiple={true}
+        />
       </div>
     </div>
   );
--- src/components/MiniCookie.jsx
+++ src/components/MiniCookie.jsx
@@ -1,23 +1,25 @@
-// import black from "./../assets/img/black_coockie.png";
-// import cyan from "./../assets/img/cyan_coockie.png";
-// import green from "./../assets/img/green_coockie.png";
-// import red from "./../assets/img/red_coockie.png";
-// import white from "./../assets/img/white_coockie.png";
-import yellow from "./../assets/img/yellow_coockie.png";
+import { motion } from "framer-motion";
 
-const MiniCookie = ({booster,color, valor, multiple}) => {
-
-    const colored = "whitecoockie";
-
-    return (
-        <div className="cookieBooster">
-            <button className="secondButton" onClick={booster}>
-                <img className="secondCookie" draggable="false" src={yellow} />
-            </button>
-            {multiple ? <p>+{valor} 🍪⏲️</p> : <p>+{valor} 🍪🖱️</p>}
-            {multiple ? <p>Cost : {valor * 100} 🍪</p> : <p>Cost : {valor * 10} 🍪</p>}
-        </div>
-    )
-}
+const MiniCookie = ({ booster, color, valor, multiple }) => {
+  return (
+    <div className="cookieBooster">
+      <button className="secondButton" onClick={booster}>
+        <motion.img
+          className="secondCookie"
+          draggable="false"
+          src={require(`./../assets/img/${color}_coockie.png`)}
+          whileTap={{ scale: 0.8 }}
+          alt="cookiebooster"
+        />
+      </button>
+      {multiple ? <p>+{valor} 🍪⏲️</p> : <p>+{valor} 🍪🖱️</p>}
+      {multiple ? (
+        <p>Cost : {valor * 100} 🍪</p>
+      ) : (
+        <p>Cost : {valor * 10} 🍪</p>
+      )}
+    </div>
+  );
+};
 
 export default MiniCookie;
